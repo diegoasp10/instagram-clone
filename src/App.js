7d176c9073558b5127acccba184ceaaaf9918592
diego.saavedra <diego.saavedra@nuvu.cc>
2024-09-22 01:33:00 -0500

    adding sides app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import LeftSide from './components/leftSide';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="leftSide">
+          <LeftSide/>
+        </div>
+        <div className="middleSide">
+          Middle Side
+        </div>
+        <div className="rightSide">
+          Right Side
+        </div>
     </div>
   );
 }
---
+++ src/components/leftSide.js
@@ -0,0 +1,12 @@
+import './leftSide.css';
+import React from 'react';
+
+const LeftSide = () => {
+    return (
+        <div>
+            Left Side
+        </div>
+    );
+};
+
+export default LeftSide
